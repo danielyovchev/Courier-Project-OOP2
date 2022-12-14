99e1107755b68f33595e2513b22db23e1e244a23
danielyovchev <daniel.yovchev@gmail.com>
2022-12-14 16:19:06 +0200

    logout

--- projectCourier/presentation/src/main/java/project/courier/presentation/controller/AdminController.java
+++ projectCourier/presentation/src/main/java/project/courier/presentation/controller/AdminController.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
+import project.courier.presentation.HelloApplication;
 import project.courier.presentation.logConstants.CurrentUser;
 
 import java.io.IOException;
@@ -58,9 +59,14 @@ public class AdminController {
         stage.show();
     }
     @FXML
-    public void logOut(){
+    public void logOut() throws IOException{
         Stage stage = (Stage) logOutBtn.getScene().getWindow();
         stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/project/courier/presentation/Login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
         CurrentUser.role="none";
     }
 }
--- projectCourier/presentation/src/main/java/project/courier/presentation/controller/CourierController.java
+++ projectCourier/presentation/src/main/java/project/courier/presentation/controller/CourierController.java
@@ -1,17 +1,27 @@
 package project.courier.presentation.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import project.courier.presentation.HelloApplication;
 import project.courier.presentation.logConstants.CurrentUser;
+
+import java.io.IOException;
 
 public class CourierController {
     @FXML
     private Button logOutBtn;
     @FXML
-    public void logOut(){
+    public void logOut() throws IOException {
         Stage stage = (Stage) logOutBtn.getScene().getWindow();
         stage.close();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/project/courier/presentation/Login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 450);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
         CurrentUser.role="none";
     }
 }
