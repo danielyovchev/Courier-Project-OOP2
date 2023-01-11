b8422e6d2aee5898166c0db56caddc6655c8c665
borisivaylov <boris_ivaylov@abv.bg>
2023-01-11 16:54:04 +0200

    Add Tableview

---
+++ projectCourier/presentation/src/main/java/project/courier/presentation/controller/AdminStatistics.java
@@ -0,0 +1,13 @@
+package project.courier.presentation.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminStatistics implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
