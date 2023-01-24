13c7da74932e6233278779416c3860f39d454806
danielyovchev <daniel.yovchev@gmail.com>
2023-01-24 15:18:26 +0200

    receive shipment feature finished

---
+++ projectCourier/presentation/src/main/java/project/courier/presentation/services/ShipmentReceiveInjector.java
@@ -0,0 +1,7 @@
+package project.courier.presentation.services;
+
+import project.courier.service.interfaces.ShipmentReceive;
+
+public interface ShipmentReceiveInjector {
+    ShipmentReceive getShipmentReceive();
+}
